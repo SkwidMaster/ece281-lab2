fcbb56b2ca53d584bd3c0d497f0d2888dfeff7bb
SkwidMaster <c27william.kozma@afacademy.af.edu>
2026-02-24 14:32:16 -0700

    Maybe this one works?

--- src/sevenseg_decoder.vhd
+++ src/sevenseg_decoder.vhd
@@ -15,22 +15,22 @@ begin
     begin
         case i_Hex is
             --        abcdefg
-            when "0000" => o_seg_n <= "0000001"; -- 0
-            when "0001" => o_seg_n <= "1001111"; -- 1
-            when "0010" => o_seg_n <= "0010010"; -- 2
-            when "0011" => o_seg_n <= "0000110"; -- 3
-            when "0100" => o_seg_n <= "1001100"; -- 4
-            when "0101" => o_seg_n <= "0100100"; -- 5
-            when "0110" => o_seg_n <= "0100000"; -- 6
-            when "0111" => o_seg_n <= "0001111"; -- 7
+            when "0000" => o_seg_n <= "1000000"; -- 0
+            when "0001" => o_seg_n <= "1111001"; -- 1
+            when "0010" => o_seg_n <= "0100100"; -- 2
+            when "0011" => o_seg_n <= "0110000"; -- 3
+            when "0100" => o_seg_n <= "0011001"; -- 4
+            when "0101" => o_seg_n <= "0010010"; -- 5
+            when "0110" => o_seg_n <= "0000010"; -- 6
+            when "0111" => o_seg_n <= "1111000"; -- 7
             when "1000" => o_seg_n <= "0000000"; -- 8
-            when "1001" => o_seg_n <= "0000100"; -- 9
+            when "1001" => o_seg_n <= "0011000"; -- 9
             when "1010" => o_seg_n <= "0001000"; -- A
-            when "1011" => o_seg_n <= "1100000"; -- b
-            when "1100" => o_seg_n <= "0110001"; -- C
-            when "1101" => o_seg_n <= "1000010"; -- d
-            when "1110" => o_seg_n <= "0110000"; -- E
-            when "1111" => o_seg_n <= "0111000"; -- F
+            when "1011" => o_seg_n <= "0000011"; -- b
+            when "1100" => o_seg_n <= "1000110"; -- C
+            when "1101" => o_seg_n <= "0100001"; -- d
+            when "1110" => o_seg_n <= "0000110"; -- E
+            when "1111" => o_seg_n <= "0001110"; -- F
             when others => o_seg_n <= "1111111"; -- all segments off
         end case;
     end process;
